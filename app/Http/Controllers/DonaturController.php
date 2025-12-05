8b9f51e3a81754119b2bf35a3585a20050f2d275
zuikoten <zuikoten@gmail.com>
2025-12-05 15:12:05 +0700

    edit role

--- app/Http/Controllers/DonaturController.php
+++ app/Http/Controllers/DonaturController.php
@@ -11,7 +11,7 @@ class DonaturController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
-        $this->middleware('role:donatur');
+        //$this->middleware('role:donatur');
     }
 
     /**
